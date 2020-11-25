85b3ca6e445c839ff4f72a9d816cb0f0e4fba281
Fanjin Zhang <zfjsail@163.com>
2020-11-25 17:24:22 +0800

    eval fit

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ from sklearn import metrics
 from embedding import EmbedMeanField, EmbedLoopyBP
 from util import cmd_args, load_data, load_self_data
 
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import precision_recall_curve
+
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s') # include timestamp
+
 
 sys.path.append(
     '%s/pytorch_structure2vec-master/s2v_lib' % os.path.dirname(
@@ -170,6 +179,60 @@ def loop_dataset(g_list, classifier, sample_idxes, optimizer=None,
     return avg_loss
 
 
+def evaluate(g_list, classifier, sample_idxes, bsize=cmd_args.batch_size, thr=None, return_best_thr=False):
+    total_iters = (len(sample_idxes) + (bsize - 1) * (optimizer is None)) // bsize # noqa
+    pbar = tqdm(range(total_iters), unit='batch')
+
+    total = 0
+    y_true, y_pred, y_score = [], [], []
+    losses = []
+
+    for pos in pbar:
+        selected_idx = sample_idxes[pos * bsize: (pos + 1) * bsize]
+
+        batch_graph = [g_list[idx] for idx in selected_idx]
+        targets = [g_list[idx].label for idx in selected_idx]
+        # all_targets += targets
+        out, loss, acc = classifier(batch_graph)
+        # all_scores.append(logits[:, 1].detach())  # for binary classification
+
+        loss = loss.data.cpu().numpy()
+        pbar.set_description('loss: %0.5f acc: %0.5f' % (loss, acc))
+
+        # total_loss.append(np.array([loss, acc]) * len(selected_idx))
+        losses.append(loss)
+
+        y_true += targets
+        y_pred += out.max(1)[1].data.tolist()
+        y_score += out[:, 1].data.tolist()
+
+        total += len(selected_idx)
+
+    if thr is not None:
+        logger.info("using threshold %.4f", thr)
+        y_score = np.array(y_score)
+        y_pred = np.zeros_like(y_score)
+        y_pred[y_score > thr] = 1
+
+    prec, rec, f1, _ = precision_recall_fscore_support(y_true, y_pred, average="binary")
+    auc = roc_auc_score(y_true, y_score)
+    logger.info("loss: %.4f AUC: %.4f Prec: %.4f Rec: %.4f F1: %.4f",
+                sum(losses) / total, auc, prec, rec, f1)
+    loss_ret = sum(losses) / total
+
+    if return_best_thr:
+        precs, recs, thrs = precision_recall_curve(y_true, y_score)
+        f1s = 2 * precs * recs / (precs + recs)
+        f1s = f1s[:-1]
+        thrs = thrs[~np.isnan(f1s)]
+        f1s = f1s[~np.isnan(f1s)]
+        best_thr = thrs[np.argmax(f1s)]
+        logger.info("best threshold=%4f, f1=%.4f", best_thr, np.max(f1s))
+        return loss_ret, [prec, rec, f1, auc], best_thr
+    else:
+        return loss_ret, [prec, rec, f1, auc], None
+
+
 if __name__ == '__main__':
     print(cmd_args)
     random.seed(cmd_args.seed)
@@ -178,14 +241,14 @@ if __name__ == '__main__':
     cmd_args.data = "twitter"
 
     # train_graphs, test_graphs = load_data()
-    train_graphs, _, test_graphs = load_self_data(cmd_args)
+    train_graphs, valid_graphs, test_graphs = load_self_data(cmd_args)
     print("attr dim", cmd_args.attr_dim)
     print("---------------------")
-    print('# train: %d, # test: %d' % (len(train_graphs), len(test_graphs)))
+    print('# train: %d, valid: %d, # test: %d' % (len(train_graphs), len(valid_graphs), len(test_graphs)))
 
     if cmd_args.sortpooling_k <= 1:
         num_nodes_list = sorted([
-            g.num_nodes for g in train_graphs + test_graphs])
+            g.num_nodes for g in train_graphs + valid_graphs + test_graphs])
         cmd_args.sortpooling_k = num_nodes_list[
             int(math.ceil(cmd_args.sortpooling_k * len(num_nodes_list))) - 1]
         cmd_args.sortpooling_k = max(10, cmd_args.sortpooling_k)
@@ -202,6 +265,14 @@ if __name__ == '__main__':
     train_idxes = list(range(len(train_graphs)))
     best_loss = None
     max_acc = 0.0
+
+    val_loss, val_metrics, thr = evaluate(valid_graphs, classifier, list(range(len(valid_graphs))),
+                                          return_best_thr=True)
+    print("validation loss:", val_loss, "metrics", val_metrics, "thr:", thr)
+
+    test_loss, test_metrics, _ = evaluate(test_graphs, classifier, list(range(len(test_graphs))), thr=thr)
+    print("test loss:", test_loss, "metrics", test_metrics)
+
     for epoch in range(cmd_args.num_epochs):
         random.shuffle(train_idxes)
         classifier.train()
@@ -209,32 +280,44 @@ if __name__ == '__main__':
             train_graphs, classifier, train_idxes, optimizer=optimizer)
         if not cmd_args.printAUC:
             avg_loss[2] = 0.0
-        print('\033[92maverage training of epoch %d: loss %.5f acc %.5f auc %.5f\033[0m' % (epoch, avg_loss[0], avg_loss[1], avg_loss[2])) # noqa
+        print('\033[92m\naverage training of epoch %d: loss %.5f acc %.5f auc %.5f\033[0m' % (epoch, avg_loss[0], avg_loss[1], avg_loss[2])) # noqa
 
         classifier.eval()
-        test_loss = loop_dataset(
-            test_graphs, classifier, list(range(len(test_graphs))))
-        if not cmd_args.printAUC:
-            test_loss[2] = 0.0
-        print('\033[93maverage test of epoch %d: loss %.5f acc %.5f auc %.5f\033[0m' % (epoch, test_loss[0], test_loss[1], test_loss[2])) # noqa
-        max_acc = max(max_acc, test_loss[1])
+        # test_loss = loop_dataset(
+        #     test_graphs, classifier, list(range(len(test_graphs))))
+        val_loss, val_metrics, thr = evaluate(valid_graphs, classifier, list(range(len(valid_graphs))), return_best_thr=True)
+        print("\nvalidation loss:", val_loss, "metrics", val_metrics, "thr:", thr)
 
-    with open('acc_result.txt', 'a+') as f:
-        # f.write(str(test_loss[1]) + '\n')
-        f.write(str(max_acc) + '\n')
+        test_loss, test_metrics, _ = evaluate(test_graphs, classifier, list(range(len(test_graphs))), thr=thr)
+        print("\ntest loss:", test_loss, "metrics", test_metrics)
 
-    if cmd_args.printAUC:
-        with open('auc_results.txt', 'a+') as f:
-            f.write(str(test_loss[2]) + '\n')
+        # if not cmd_args.printAUC:
+        #     test_loss[2] = 0.0
+        # print('\033[93maverage test of epoch %d: loss %.5f acc %.5f auc %.5f\033[0m' % (epoch, test_loss[0], test_loss[1], test_loss[2])) # noqa
+        # max_acc = max(max_acc, test_loss[1])
+        if val_metrics[-1] > max_acc:
+            max_acc = val_metrics[-1]
+            best_thr = thr
+            best_valid_metrics = val_metrics
+            best_test_metrics = test_metrics
 
-    if cmd_args.extract_features:
-        features, labels = classifier.output_features(train_graphs)
-        labels = labels.type('torch.FloatTensor')
-        np.savetxt('extracted_features_train.txt', torch.cat(
-            [labels.unsqueeze(1), features.cpu()], dim=1).detach().numpy(),
-                '%.4f')
-        features, labels = classifier.output_features(test_graphs)
-        labels = labels.type('torch.FloatTensor')
-        np.savetxt('extracted_features_test.txt', torch.cat(
-            [labels.unsqueeze(1), features.cpu()], dim=1).detach().numpy(),
-                '%.4f')
+
+    # with open('acc_result.txt', 'a+') as f:
+    #     f.write(str(test_loss[1]) + '\n')
+        # f.write(str(max_acc) + '\n')
+
+    # if cmd_args.printAUC:
+    #     with open('auc_results.txt', 'a+') as f:
+    #         f.write(str(test_loss[2]) + '\n')
+
+    # if cmd_args.extract_features:
+    #     features, labels = classifier.output_features(train_graphs)
+    #     labels = labels.type('torch.FloatTensor')
+    #     np.savetxt('extracted_features_train.txt', torch.cat(
+    #         [labels.unsqueeze(1), features.cpu()], dim=1).detach().numpy(),
+    #             '%.4f')
+    #     features, labels = classifier.output_features(test_graphs)
+    #     labels = labels.type('torch.FloatTensor')
+    #     np.savetxt('extracted_features_test.txt', torch.cat(
+    #         [labels.unsqueeze(1), features.cpu()], dim=1).detach().numpy(),
+    #             '%.4f')
